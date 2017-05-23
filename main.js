23b6a3711dbfee775e62e424aae20feb519977b8
Gerald Devreux <gerald.devreux.pro@gmail.com>
2017-05-23 09:52:49 +0200

    cesar v1.1

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
+// INIT DES VARIABLES
+var userData;
 var a = baffle('#testID');
+var tab = [];
+var nbrIncrementation = 0;
+let newVar = "";
 
+// Function which increment the letters
+function nextChar(c, nbrIncrementation)
+{
+	return 	String.fromCharCode(c.charCodeAt(0) + nbrIncrementation);
+}
+
+// Function LAUNCHED when button SUBMIT CLICKED
 function getText(){
 
-	var userData = document.getElementById("data").value;
+	userData = document.getElementById("data").value;
+	nbrIncrementation = parseInt(document.getElementById("nbrCesar").value);
 	
-	document.getElementsByClassName("baffle")[0].innerHTML += "Cryptage en cours ...";
-	//document.getElementsByClassName("baffle")[0].innerHTML += userData;
+	document.getElementsByClassName("baffle")[0].innerHTML += "Cryptage en cours ..." + "<br>";
+	document.getElementsByClassName("baffle")[0].innerHTML += "Mot avant cryptage : " + userData + "<br>";
+	
 
 	document.getElementById("testID").innerHTML += userData;
 
 	a.start();
-	// // a.stop();
-	a.reveal(20000);
+	a.stop();
+	a.reveal(5000);
 
-	console.log(userData);
+	console.log(userData[0]);
+	console.log("nbrIncrement : ", nbrIncrementation);
+	let nbrTest = 2;
+	for (let i = 0; i < userData.length; i++){
+		console.log(nbrIncrementation);
+		newVar += nextChar(userData[i], nbrIncrementation);	
+	}
+
+	document.getElementsByClassName("baffle")[0].innerHTML += "Mot après cryptage : " + newVar + "<br>";
+	
+	
+	console.log("variable cryptée : " + newVar);
+	document.getElementById("testID").innerHTML += newVar;
+	newVar = "";
 
 }
+
+
+
+
 
 // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
